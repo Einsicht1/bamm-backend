f26b4d1809b36f009c602e285343a28356d5c248
Einsicht1 <kpl5672@gmail.com>
2023-04-11 23:50:48 +0900

    front local에서 붙을 수 있도록 cors 설정

--- bamm/settings/base.py
+++ bamm/settings/base.py
@@ -155,7 +155,7 @@ MEDIAFILES_LOCATION = "media"
 CORS_ORIGIN_WHITELIST = [
     "https://bamm.kr",
     "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
+    "http://localhost:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
