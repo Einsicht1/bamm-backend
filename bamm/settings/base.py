536a8a72177eaaa2a67ddce6536cb8dab3d2ba7e
Einsicht1 <kpl5672@gmail.com>
2023-04-17 16:53:47 +0900

    sentry 설정 변경

--- bamm/settings/base.py
+++ bamm/settings/base.py
@@ -13,9 +13,6 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -221,18 +218,3 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
-
-
-sentry_sdk.init(
-    dsn="https://e33994c42b764c48bd44215bac0d1a09@o4504989486809088.ingest.sentry.io/4504989492838400",
-    integrations=[
-        DjangoIntegration(),
-    ],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-)
--- bamm/settings/live.py
+++ bamm/settings/live.py
@@ -1,5 +1,8 @@
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from .base import *
 
 DEBUG = False
@@ -19,3 +22,19 @@ DATABASES = {
         "PASSWORD": os.environ.get("PASSWORD"),
     }
 }
+
+
+sentry_sdk.init(
+    dsn="https://e33994c42b764c48bd44215bac0d1a09@o4504989486809088.ingest.sentry.io/4504989492838400",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    environment="production",
+)
